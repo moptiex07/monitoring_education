8cbe77407d35b76767f519d1da3a07bbd6b60238
moptiex_07 <max.yarkhunin@gmail.com>
2023-06-22 18:22:27 +0600

    Add security group

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "app01" {
   ami           = "ami-022e1a32d3f742bd8"  
   instance_type = "t2.micro"  
   key_name = "main"  
-
+  vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
     Name = "app01"  
   }
@@ -18,7 +18,7 @@ resource "aws_instance" "mon01" {
   ami           = "ami-022e1a32d3f742bd8"
   instance_type = "t2.micro"
   key_name = "main"
-
+  vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
     Name = "mon01"                
   }
@@ -29,9 +29,31 @@ resource "aws_instance" "prom01" {
   ami           = "ami-022e1a32d3f742bd8"
   instance_type = "t2.micro"
   key_name = "main"
-
+  vpc_security_group_ids = [aws_security_group.sg.id]
   tags = {
     Name = "prom01"                
   }
 }
 
+resource "aws_security_group" "sg" {
+  name        = "my_security_group"
+  description = "Allow all incoming and outgoing traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "my_security_group"
+  }
+}
