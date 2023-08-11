4a167706d88f4e554e81f70616efe376f32f7c5e
max yarkh <max.yarkhunin@gmail.com>
2023-08-11 20:11:34 +0600

    another bugs with vars ansible

--- terraform/add_ip_to_inventory.sh
+++ terraform/add_ip_to_inventory.sh
@@ -8,14 +8,17 @@ cat > ../ansible/inventory.ini <<EOF
 [all:vars]
 ansible_user=ec2-user
 ansible_ssh_private_key_file=/Users/maksimyarhunin/Downloads/main.pem
+app01_ip=${APP01_IP}
+mon01_ip=${MON01_IP}
+prom01_ip=${PROM01_IP}
 
 [appservers]
-app01 ansible_host=${APP01_IP} app01_ip=${APP01_IP}
+app01 ansible_host=${APP01_IP}
 
 [monservers]
-mon01 ansible_host=${MON01_IP} mon01_ip=${MON01_IP}
+mon01 ansible_host=${MON01_IP}
 
 [prometheus]
-prom01 ansible_host=${PROM01_IP} prom01_ip=${PROM01_IP}
+prom01 ansible_host=${PROM01_IP}
 EOF
 
